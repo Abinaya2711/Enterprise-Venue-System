6ab5016c1a186d4cf779fb72fdd7f1a7b1e867c9
Ajithajothiraj <ajithajothiraj@gmail.com>
2025-02-14 21:57:24 +0530

    Admin page added

---
+++ src/Admin.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import HomePage from './HomePage';
+import './Admin.css'
+
+const Admin = () => {
+    const navigate=useNavigate()
+    const handleChange=(event)=>{
+        event.preventDefault();
+        navigate('/homepage')
+    }
+  return (
+    <div>
+      <div className="container">
+        <h2>Admin Login</h2>
+        <form  className="login-form" onSubmit={handleChange}>
+
+            <label htmlFor="email">Email ID:</label>
+            <input type="email" id="email" name="email" required /><br/>
+
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required /><br/>
+
+            <button type="submit" >Login</button>
+        </form>
+        <p  className="forgot-password-link" >Forgot Password?<span> Click here</span></p>
+    </div>
+    </div>
+  )
+}
+
+export default Admin
