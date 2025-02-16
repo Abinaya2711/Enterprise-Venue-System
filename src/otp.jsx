a62f6d6b261542bbbc0165cc62b444bd8bd82d3b
ALAGUANUSHIYA <alaguanushiyar@gmail.com>
2025-02-16 18:47:35 +0530

    Fixed forgot password, OTP, and reset password components

--- src/otp.jsx
+++ src/otp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/OTP.css";
+import "./OTP.css";
 
 function OTP() {
   const [otp, setOtp] = useState("");
