5bde93f3c2b7876fc585172dca51555f0ea07b2b
ALAGUANUSHIYA <alaguanushiyar@gmail.com>
2025-02-16 18:36:38 +0530

    Fixed file imports and updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Admin from './Admin'
-import HomePage from './HomePage'
-import People from './People'
-import Hall from './Hall'
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Admin from './Admin';
+import HomePage from './HomePage';
+import People from './People';
+import Hall from './Hall';
+import Login from './login';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Admin />} />
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/people" element={<People />} />
-        <Route path="/hall" element={<Hall />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/otp" element={<OTP />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-      </Routes>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Admin />} />  {/* Set Admin as default */}
+      <Route path="/login" element={<Login />} />  {/* Fixed path */}
+      <Route path="/homepage" element={<HomePage />} />
+      <Route path="/people" element={<People />} />
+      <Route path="/hall" element={<Hall />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/otp" element={<OTP />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
