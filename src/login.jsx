d89faf8e8b18ae9c4f2b1e7e4c547180f0a535c7
ALAGUANUSHIYA <alaguanushiyar@gmail.com>
2025-02-16 18:45:00 +0530

    Fixed file imports and updated styles

--- src/login.jsx
+++ src/login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/Login.css";
+import "./login.css";
+
 
 function Login() {
   const [email, setEmail] = useState("");
